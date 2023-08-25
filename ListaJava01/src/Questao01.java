4f2264c04b4f6c7f2e4808c4620dca52e714c691
João Victor <joaovba2002@alu.ufc.br>
2023-08-25 12:20:44 -0300

    questao 01 a 4

---
+++ ListaJava01/src/Questao01.java
@@ -0,0 +1,10 @@
+
+public class Questao01 {
+
+	public static void main(String[] args) {
+		// 1. Faça um programa na linguagem Java que mostre a mensagem “Alô mundo!” na tela.
+		System.out.println("Alô mundo!");
+
+	}
+
+}
